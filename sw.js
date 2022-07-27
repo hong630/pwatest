cfea1064c2af45c8c742cdb2d20aac26200a1696
hong630 <hongihongi60@gmail.com>
2022-07-27 23:39:38 +0900

    d

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
 self.addEventListener('fetch', function(e) {
     // 여기에 코드를 넣지 않아도 설치는 가능하다.
+
+
+
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt = e;
+    });
+
+    const installApp = document.getElementById('installApp');
+
+    installApp.addEventListener('click', async () => {
+        if (deferredPrompt !== null) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                deferredPrompt = null;
+            }
+        }
+    });
+
 })
