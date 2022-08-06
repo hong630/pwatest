eb0d47f39c5b571588211c9d1d996f2d83699aeb
hong630 <hongihongi60@gmail.com>
2022-08-06 23:47:33 +0900

    discord

--- test.js
+++ test.js
@@ -1,48 +1,49 @@
+//
+// function changeTxt(){
+//     const testTxt = document.getElementById('original_text').value;
+//     const arr1 = testTxt.split(' ');
+//     const englishReg = /^[A-Za-z][A-Za-z0-9]*$/;
+//     let standardNum = document.getElementById('random_value').value;
+//     let arr2 = [];
+//     let word;
+//     for(let i=0; i<arr1.length; i++){
+//         if(i%standardNum === 0 && englishReg.test(arr1[i])){
+//             word = `_________`
+//         }else{
+//             word = arr1[i];
+//         }
+//         arr2.push(word);
+//     }
+//
+//     let result = ''
+//     for(let j=0; j<arr2.length; j++){
+//         result = result + arr2[j] + ' '
+//     }
+//     console.log(result);
+//     const blankedDiv = document.getElementById('blanked_text');
+//     blankedDiv.innerText = result;
+// }
+//
+// let deferredPrompt;
+//
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     deferredPrompt = e;
+// });
+//
+// const installApp = document.getElementById('installApp');
+// console.log(installApp)
+//
+// installApp.addEventListener('click', async () => {
+//     console.log('working?')
+//     if (deferredPrompt !== null) {
+//         deferredPrompt.prompt();
+//         const { outcome } = await deferredPrompt.userChoice;
+//         if (outcome === 'accepted') {
+//             deferredPrompt = null;
+//         }
+//     }
+// });
 
-function changeTxt(){
-    const testTxt = document.getElementById('original_text').value;
-    const arr1 = testTxt.split(' ');
-    const englishReg = /^[A-Za-z][A-Za-z0-9]*$/;
-    let standardNum = document.getElementById('random_value').value;
-    let arr2 = [];
-    let word;
-    for(let i=0; i<arr1.length; i++){
-        if(i%standardNum === 0 && englishReg.test(arr1[i])){
-            word = `_________`
-        }else{
-            word = arr1[i];
-        }
-        arr2.push(word);
-    }
-
-    let result = ''
-    for(let j=0; j<arr2.length; j++){
-        result = result + arr2[j] + ' '
-    }
-    console.log(result);
-    const blankedDiv = document.getElementById('blanked_text');
-    blankedDiv.innerText = result;
-}
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    deferredPrompt = e;
-});
-
-const installApp = document.getElementById('installApp');
-console.log(installApp)
-
-installApp.addEventListener('click', async () => {
-    console.log('working?')
-    if (deferredPrompt !== null) {
-        deferredPrompt.prompt();
-        const { outcome } = await deferredPrompt.userChoice;
-        if (outcome === 'accepted') {
-            deferredPrompt = null;
-        }
-    }
-});
 
 
 
